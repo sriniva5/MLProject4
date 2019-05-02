222336c80deb279cc9b7d2a86606517d41adea90
Kevin Ackerman <ackermk3@tcnj.edu>
2019-05-02 17:37:57 -0400

    read in data

--- learner.py
+++ learner.py
@@ -1,6 +1,23 @@
 import perceptron
+
+def fileread():
+	with open("iris.data", "r") as f:
+		lines = f.read().splitlines()
+		attributes = lines[0].split(",")[0:-1]
+		target_attribute = lines[0].split(",")[-1]
+
+	training_data = []
+
+	for line in lines[0:-1]:
+		training_data.append(line.split(","))
+
+	return training_data 
+
 def main():
 	irisDat = fileread()
-	ann = createann()
-	ann = initann(ann)
-	bpa(ann, irisDat)
+	print(irisDat)
+	#ann = createann()
+	#ann = initann(ann)
+	#bpa(ann, irisDat)
+
+main()
