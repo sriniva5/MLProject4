afc4241bdbd9797c9d91711d26062bad984a684c
chris weeden <mrhalofan25@gmail.com>
2019-05-02 17:12:04 -0400

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -13,15 +13,22 @@ class Perceptron():
 		output = self.child_weights[0]
 		for i, x in enumerate(children):
 			output+=x.data*self.child_weights[i]
+		if output <= 0:
+			output = 0
+		else:
+			output = 1
 		self.data = output
 	def add_child(self, obj):
-		self.children.append(obj)
+		for x in obj:
+			self.children.append(x)
 	def add_parent(self, obj):
-		self.parent.append(obj)
+		for x in obj:
+			self.parent.append(x)
 	def set_weights(self, weights):
 		self.child_weights = weights.copy()
 	def init_weights(self):
 		if len(children)<=0:
 			return
 		for x in range(len(children)+1):
-			self.child_weights.append(random.range(-0.05,0.05))
+			self.child_weights.apend(0)
+			#self.child_weights.append(random.range(-0.05,0.05))
