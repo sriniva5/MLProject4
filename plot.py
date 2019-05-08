cec5879b13a0a288a310df8601750d68a2ed833b
sriniva5 <aasrin99@gmail.com>
2019-05-08 17:39:52 -0400

    add plots for all tasks

--- plot.py
+++ plot.py
@@ -43,3 +43,213 @@ if __name__ == "__main__":
     plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
     plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
     plt.clf()
+
+    file = "./weight1/setosa1.txt"
+    type = "setosa"
+    weight_type = "weights = 1"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./weight1/versicolor1.txt"
+    type = "versicolor"
+    weight_type = "weights = 1"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./weight1/virginica1.txt"
+    type = "virginica"
+    weight_type = "weights = 1"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./weightrand0/setosarand0.txt"
+    type = "setosa"
+    weight_type = "random weights"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./weightrand0/versicolorrand0.txt"
+    type = "versicolor"
+    weight_type = "random weights"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./weightrand0/virginicarand0.txt"
+    type = "virginica"
+    weight_type = "random weights"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./weightrand1/setosarand1.txt"
+    type = "setosa"
+    weight_type = "random weights"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./weightrand1/versicolorrand1.txt"
+    type = "versicolor"
+    weight_type = "random weights"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./weightrand1/virginicarand1.txt"
+    type = "virginica"
+    weight_type = "random weights"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./shuff0/setosashuff0.txt"
+    type = "setosa"
+    weight_type = "shuffled dataset"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./shuff0/versicolorshuff0.txt"
+    type = "versicolor"
+    weight_type = "shuffled dataset"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./shuff0/virginicashuff0.txt"
+    type = "virginica"
+    weight_type = "shuffled dataset"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./shuff1/setosashuff1.txt"
+    type = "setosa"
+    weight_type = "shuffled dataset"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./shuff1/versicolorshuff1.txt"
+    type = "versicolor"
+    weight_type = "shuffled dataset"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
+
+    file = "./shuff1/virginicashuff1.txt"
+    type = "virginica"
+    weight_type = "shuffled dataset"
+
+    data = pd.read_csv(file, sep=",", header=None)
+    data.columns = ["epoch", "errors", "w0", "w1", "w2", "w3", "w4"]
+
+    plt.plot(data['epoch'].values, data['errors'].values, 'ro')
+    plt.xlabel('Epoch #')
+    plt.ylabel('# of Errors in Learning')
+    plt.title('Number of Learning Errors made per Epoch (' + type + ', ' + weight_type + ')')
+    plt.savefig(file[0:-4]+".pdf", bbox_inches="tight")
+    plt.clf()
